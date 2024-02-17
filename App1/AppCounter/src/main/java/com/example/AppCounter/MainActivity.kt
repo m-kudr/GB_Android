db422ab2758bdf5713a456b6920894ad4a71ff5a
mkudr <mkudr29@google.com>
2024-02-17 20:59:46 +0300

    Fix checkNumber method

--- App1/AppCounter/src/main/java/com/example/AppCounter/MainActivity.kt
+++ App1/AppCounter/src/main/java/com/example/AppCounter/MainActivity.kt
@@ -9,35 +9,35 @@ import com.example.AppCounter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val minNumber: Int = 0
     private var currentNumber: Int = 0
-    private val maxNumber: Int = 5
+    private val maxNumber: Int = 50
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textView.text = "Все места свободны"
         binding.buttonMinus.isEnabled = false
+        binding.count.text = "0"
+        binding.buttonReset.visibility = View.INVISIBLE
 
         fun checkCount(count: Int) {
             binding.count.text = currentNumber.toString()
             binding.buttonReset.visibility = View.INVISIBLE
-            when (count) {
-                minNumber -> {
-                    binding.buttonReset.visibility = View.INVISIBLE
-                    binding.buttonMinus.isEnabled = false
-                    binding.buttonPlus.isEnabled = true
-                    binding.textView.text = "Все места свободны"
-                    binding.textView.setTextColor(Color.rgb(0, 150, 136))
-                }
-                maxNumber -> {
-                    binding.buttonReset.visibility = View.VISIBLE
-                    binding.buttonPlus.isEnabled = false
-                    binding.textView.text = "Пассажиров слишком много!"
-                    binding.textView.setTextColor(Color.RED)
-                }
-                else -> {
+            if (count == minNumber) {
+                binding.buttonReset.visibility = View.INVISIBLE
+                binding.buttonMinus.isEnabled = false
+                binding.textView.text = "Все места свободны"
+                binding.textView.setTextColor(Color.rgb(0, 150, 136))
+            } else {
+                if (count < maxNumber) {
                     binding.buttonMinus.isEnabled = true
-                    binding.buttonPlus.isEnabled = true
                     binding.textView.text = "Осталось мест: " + (maxNumber - count)
                     binding.textView.setTextColor(Color.BLUE)
+                } else {
+                    binding.buttonReset.visibility = View.VISIBLE
+                    binding.textView.text = "Пассажиров слишком много!"
+                    binding.textView.setTextColor(Color.RED)
                 }
             }
         }
