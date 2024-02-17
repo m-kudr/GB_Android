b4fe4655fc385da02af2a8fd89a63eb846afcba1
mkudr <mkudr29@google.com>
2024-02-17 20:03:36 +0300

    Fix variable place. Add enable|disable for buttons on count state.

--- App1/AppCounter/src/main/java/com/example/AppCounter/MainActivity.kt
+++ App1/AppCounter/src/main/java/com/example/AppCounter/MainActivity.kt
@@ -7,28 +7,35 @@ import android.view.View
 import com.example.AppCounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val minNumber: Int = 0
+    private var currentNumber: Int = 0
+    private val maxNumber: Int = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val minNumber = 0
-        var currentNumber = 0
-        val maxNumber = 50
+        binding.buttonMinus.isEnabled = false
 
         fun checkCount(count: Int) {
             binding.count.text = currentNumber.toString()
             binding.buttonReset.visibility = View.INVISIBLE
             when (count) {
-                0 -> {
+                minNumber -> {
+                    binding.buttonReset.visibility = View.INVISIBLE
+                    binding.buttonMinus.isEnabled = false
+                    binding.buttonPlus.isEnabled = true
                     binding.textView.text = "Все места свободны"
                     binding.textView.setTextColor(Color.rgb(0, 150, 136))
                 }
                 maxNumber -> {
+                    binding.buttonReset.visibility = View.VISIBLE
+                    binding.buttonPlus.isEnabled = false
                     binding.textView.text = "Пассажиров слишком много!"
                     binding.textView.setTextColor(Color.RED)
                 }
                 else -> {
+                    binding.buttonMinus.isEnabled = true
+                    binding.buttonPlus.isEnabled = true
                     binding.textView.text = "Осталось мест: " + (maxNumber - count)
                     binding.textView.setTextColor(Color.BLUE)
                 }
@@ -41,16 +48,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonPlus.setOnClickListener {
-            if (currentNumber < maxNumber) {
-                checkCount(++currentNumber)
-                if (currentNumber == maxNumber)
-                    binding.buttonReset.visibility = View.VISIBLE
-            }
+            checkCount(++currentNumber)
         }
 
         binding.buttonMinus.setOnClickListener {
-            if (currentNumber > minNumber)
-                checkCount(--currentNumber)
+            checkCount(--currentNumber)
         }
     }
 }
