33610840c0fa3ea8d052feb9911b9d5abf5e484d
mkudr <mkudr29@google.com>
2024-03-13 23:54:14 +0300

    Add project App9

---
+++ App9/app/src/main/java/com/example/easyquiz/StartFragment.kt
@@ -0,0 +1,29 @@
+package com.example.easyquiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.easyquiz.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+    private lateinit var binding: FragmentStartBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentStartBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_quizFragment)
+        }
+    }
+}
